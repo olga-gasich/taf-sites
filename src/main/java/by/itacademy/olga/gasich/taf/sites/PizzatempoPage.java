50afc640ece9ecd428dfcbb9f011882debb61a57
Olga Gasich <olagasich@gmail.com>
2023-05-20 13:51:00 +0300

    add PizzatempoPage class

---
+++ src/main/java/by/itacademy/olga/gasich/taf/sites/PizzatempoPage.java
@@ -0,0 +1,8 @@
+package by.itacademy.olga.gasich.taf.sites;
+
+public class PizzatempoPage {
+
+    String inputEmailAddressXpath = "/html/body/div/div[1]/form/p[1]/input";
+    String inputPswXpath = "/html/body/div/div[1]/form/p[2]/input[1]";
+    String btnSearchXpath = "/html/body/div/div[1]/form/p[2]/input[2]";
+}
