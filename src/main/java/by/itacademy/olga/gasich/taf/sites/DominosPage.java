382ff47c6e64dd37fe00aa8e7828567e50253b5f
Olga Gasich <olagasich@gmail.com>
2023-05-20 15:31:46 +0300

    add DominosPage class

---
+++ src/main/java/by/itacademy/olga/gasich/taf/sites/DominosPage.java
@@ -0,0 +1,10 @@
+package by.itacademy.olga.gasich.taf.sites;
+
+public class DominosPage {
+
+    String btnCrossOnModalCss = "body > div.custom-modal.custom-modal--size-content.custom-modal--padding-none.custom-modal--bg-transparent > div.custom-modal__content-holder > button > span > span.custom-button__content-children > svg";
+    String btnEnterXpath = "//*[@id='app-root']/div/div[1]/div[1]/div/div[4]/div/div/div[3]/button/span/span[2]";
+    String inputEmailAddressXpath = "/html/body/div[4]/div[2]/div/div/div/div[2]/div[2]/div/form/div/div[1]/input";
+    String inputPswXpath = "/html/body/div[4]/div[2]/div/div/div/div[2]/div[2]/div/form/div/div[2]/input";
+    String btnSubmitXpath = "/html/body/div[4]/div[2]/div/div/div/div[2]/div[2]/div/form/div/button";
+}
