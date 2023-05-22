20bdf064534b88db3c773c77e412374c590fcbde
Olga Gasich <olagasich@gmail.com>
2023-05-22 14:02:40 +0300

    add TripadvisorPage class

---
+++ src/main/java/by/itacademy/olga/gasich/taf/sites/TripadvisorPage.java
@@ -0,0 +1,11 @@
+package by.itacademy.olga.gasich.taf.sites;
+
+public class TripadvisorPage {
+
+    String btnSignInXpath = "//*[@id='lithium-root']/header/div/nav/div/div[2]/a[3]/span";
+    String iframeCsspath = "iframe[title='regcontroller']";
+    String btnContinueWithEmailXpath = "//span[contains(text(),'Continue with email')]";
+    String inputEmailAddressXpath = "//*[@id='regSignIn.email']";
+    String inputPswXpath = "//*[@id='regSignIn.password']";
+    String btnSubmitXpath = "//*[@id='regSignIn']/div[4]/button[1]";
+}
