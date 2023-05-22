7e2629b61994aa4c3a3412f9b12ce6be07cecb8f
Olga Gasich <olagasich@gmail.com>
2023-05-22 17:37:14 +0300

    add BookingPage class

---
+++ src/main/java/by/itacademy/olga/gasich/taf/sites/BookingPage.java
@@ -0,0 +1,10 @@
+package by.itacademy.olga.gasich.taf.sites;
+
+public class BookingPage {
+
+    String btnSignInOrRegisterXpath = "//*[@id='b2indexPage']/div[21]/div/div/div/div[2]/div/a/span";
+    String inputEmailAddressXpath = "//*[@id='username']";
+    String btnContinueWithEmailXpath = "//*[@id='root']/div/div/div/div[2]/div[1]/div/div/div/div/div/div/form/div[3]/button";
+    String inputPswXpath = "//*[@id='password']";
+    String btnSubmitXpath = "//*[@id='root']/div/div/div/div[2]/div[1]/div/div/div/div/div/div/form/div[2]/button";
+}
