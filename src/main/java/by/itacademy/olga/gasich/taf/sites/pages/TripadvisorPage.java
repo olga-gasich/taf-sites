cbf53a7c2fbe9b3f556afa3f0c6fd8245112826d
Olga Gasich <olagasich@gmail.com>
2023-07-21 15:19:44 +0300

    refactor TripAdvisorPage and TripAdvisorTest classes

--- src/main/java/by/itacademy/olga/gasich/taf/sites/pages/TripadvisorPage.java
+++ src/main/java/by/itacademy/olga/gasich/taf/sites/pages/TripadvisorPage.java
@@ -3,16 +3,24 @@ package by.itacademy.olga.gasich.taf.sites.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class TripadvisorPage {
 
     private ChromeDriver driver;
-    private String btnSignInXpath = "//*[@id='lithium-root']/header/div/nav/div/div[2]/a[3]/span";
+    private String btnSignInXpath = "//span[@class='biGQs _P ttuOS' and contains(text(),'Sign in')]";
     private String iframeCsspath = "iframe[title='regcontroller']";
     private String btnContinueWithEmailXpath = "//span[contains(text(),'Continue with email')]";
     private String inputEmailAddressXpath = "//*[@id='regSignIn.email']";
     private String inputPswXpath = "//*[@id='regSignIn.password']";
     private String btnSubmitXpath = "//*[@id='regSignIn']/div[4]/button[1]";
+    private String errorEmptyEmailXpath = "//div[@class ='body_text' and contains(text(),'E-mail address is required')]";
+    private String errorInvalidEmailXpath = "/html/body/span/div[2]";
+    private String errorPswXpath = "//div[@class ='body_text' and contains(text(),'Your password must be at least 10 characters. Please try again.')]";
+    private String errorIncorrectEmailOrPswXpath = "//*[@id='regErrors']";
     private String baseURL = "https://www.tripadvisor.com/";
 
     public TripadvisorPage(ChromeDriver newDriver){
@@ -26,7 +34,8 @@ public class TripadvisorPage {
 
     public void clickBtnSignIn(){
 
-        WebElement btnSignIn = driver.findElement(By.xpath(btnSignInXpath));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement btnSignIn = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(btnSignInXpath)));
         btnSignIn.click();
     }
 
@@ -53,5 +62,37 @@ public class TripadvisorPage {
 
         WebElement btnSubmit = driver.findElement(By.xpath(btnSubmitXpath));
         btnSubmit.click();
+    }
+
+    public String getEmptyEmailError(){
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement actError = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(errorEmptyEmailXpath)));
+        String actErrorMessage = actError.getText();
+        return actErrorMessage;
+    }
+
+    public String getInvalidEmailError(){
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement actError = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(errorInvalidEmailXpath)));
+        String actErrorMessage = actError.getText();
+        return actErrorMessage;
+    }
+
+    public String getEmptyPswError(){
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement actError = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(errorPswXpath)));
+        String actErrorMessage = actError.getText();
+        return actErrorMessage;
+    }
+
+    public String getInvalidEmailOrPswError(){
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement actError = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(errorIncorrectEmailOrPswXpath)));
+        String actErrorMessage = actError.getText();
+        return actErrorMessage;
     }
 }
--- src/test/java/by/itacademy/olga/gasich/taf/sites/tests/TripadvisorTest.java
+++ src/test/java/by/itacademy/olga/gasich/taf/sites/tests/TripadvisorTest.java
@@ -4,6 +4,7 @@ import by.itacademy.olga.gasich.taf.sites.pages.TripadvisorPage;
 import by.itacademy.olga.gasich.taf.sites.steps.TripadvisorStep;
 import by.itacademy.olga.gasich.taf.sites.utils.Util;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -31,24 +32,28 @@ public class TripadvisorTest {
     public void testFillFormWithEmptyEmailAndPassword() {
 
         step.fillLoginFormAndSubmit("", "");
+        Assertions.assertEquals("E-mail address is required", page.getEmptyEmailError());
     }
 
     @Test
     public void testFillFormWithInvalidEmail() {
 
         step.fillLoginFormAndSubmit(Util.generatePassword(), "");
+        Assertions.assertEquals("E-mail address is either invalid or starts with a generic alias to which we cannot send.", page.getInvalidEmailError());
     }
 
     @Test
     public void testFillFormWithValidEmailAndEmptyPassword() {
 
         step.fillLoginFormAndSubmit(Util.generateEmail(), "");
+        Assertions.assertEquals("Your password must be at least 10 characters. Please try again.", page.getEmptyPswError());
     }
 
     @Test
     public void testFillFormWithValidEmailAndSomePassword() {
 
         step.fillLoginFormAndSubmit(Util.generateEmail(), Util.generatePassword());
+        Assertions.assertEquals("Either your email or password was incorrect. Please try again or click the \"Forgot password?\" link below.", page.getInvalidEmailOrPswError());
     }
 
     @AfterEach
